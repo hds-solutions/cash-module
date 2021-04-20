6c0eebec75acd678bbc1c43a0575991f96a37791
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-20 19:21:05 -0400

    Allow to specify if amount() is called with a raw value

--- app/helpers.php
+++ app/helpers.php
@@ -3,8 +3,8 @@
 use HDSSolutions\Finpar\Models\Currency;
 
 if (! function_exists('amount')) {
-    function amount($amount, Currency $currency) {
+    function amount($amount, Currency $currency, bool $raw = false) {
         // return formated amount
-        return $currency->code.' '.number(($amount ?? 0) / pow(10, $currency->decimals ?? 0), $currency->decimals);
+        return $currency->code.' '.number($raw ? ($amount ?? 0) : ($amount ?? 0) / pow(10, $currency->decimals ?? 0), $currency->decimals);
     }
 }
