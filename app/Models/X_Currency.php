c68a7854f2a090c981edd7740475b22bfe1b5a95
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-23 17:11:33 -0400

    Currency.isCurrent attribute

--- app/Models/X_Currency.php
+++ app/Models/X_Currency.php
@@ -20,4 +20,9 @@ abstract class X_Currency extends Base\Model {
         'decimals'  => [ 'required', 'numeric', 'min:0' ],
     ];
 
+    public function getIsCurrentAttribute():bool {
+        // return if this currency is the current in use
+        return $this->id == backend()->currency()->getKey();
+    }
+
 }
