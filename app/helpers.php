7a4adf1d44a1040e4bd3b37ad7a3f8fd4617ea97
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-06-22 19:04:13 -0400

    Helper to get cached version of currencies

--- app/helpers.php
+++ app/helpers.php
@@ -3,8 +3,17 @@
 use HDSSolutions\Finpar\Models\Currency;
 
 if (! function_exists('amount')) {
-    function amount($amount, Currency $currency, bool $raw = false) {
+    function amount($amount, int|Currency $currency, bool $raw = false) {
+        // load currency
+        $currency = $currency instanceof Currency ? $currency : currency($currency);
         // return formated amount
         return $currency->code.' '.number($raw ? ($amount ?? 0) : ($amount ?? 0) / pow(10, $currency->decimals ?? 0), $currency->decimals);
+    }
+}
+
+if (!function_exists('currency')) {
+    function currency(int $currency_id):Currency {
+        // get cached currencies
+        return backend()->currencies()->firstWhere('id', $currency_id);
     }
 }
